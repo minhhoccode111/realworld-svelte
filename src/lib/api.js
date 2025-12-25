0c49aab0581b3535a7004d1ce3810f48d797f1d5
minhhoccode111 <minhhoccode111@gmail.com>
2025-12-25 14:43:25 +0700

    chore: run locally with LLMs support

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit';
 
-const base = 'https://api.realworld.show/api';
+// const base = 'https://api.realworld.show/api';
+const base = 'http://localhost:8080/api/v1';
 
 async function send({ method, path, data, token }) {
 	const opts = { method, headers: {} };
