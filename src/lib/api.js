fd7bc20047e8bed34982362214f63fe867850858
minhhoccode111 <minhhoccode111@gmail.com>
2025-12-29 22:34:36 +0700

    fix: optimistic UI update doesn't work when toggle favorite in article preview

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 
-const base = 'https://api.realworld.show/api';
+const base = 'http://localhost:8080/api/v1';
 
 async function send({ method, path, data, token }) {
 	const opts = { method, headers: {} };
