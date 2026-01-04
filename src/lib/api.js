2da911c3a76b23ded6a939d2c937825bb3da57b6
minhhoccode111 <minhhoccode111@gmail.com>
2026-01-04 22:27:27 +0700

    Revert "chore: run locally with LLMs support"
    This reverts commit 0c49aab0581b3535a7004d1ce3810f48d797f1d5.

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,6 @@
 import { error } from '@sveltejs/kit';
 
-// const base = 'https://api.realworld.show/api';
-const base = 'http://localhost:8080/api/v1';
+const base = 'https://api.realworld.show/api';
 
 async function send({ method, path, data, token }) {
 	const opts = { method, headers: {} };
