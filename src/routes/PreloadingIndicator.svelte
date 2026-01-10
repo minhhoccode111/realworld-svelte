e0d9623cbab06ee049058c554c406a04e783ce26
minhhoccode111 <minhhoccode111@gmail.com>
2026-01-10 18:46:27 +0700

    feat: update the preloadindicator to use Tween motion and cubicOut easing

--- src/routes/PreloadingIndicator.svelte
+++ src/routes/PreloadingIndicator.svelte
@@ -1,65 +1,33 @@
 <script>
 	import { onMount } from 'svelte';
+	import { Tween } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
-	let p = $state(0);
 	let visible = $state(false);
+	let progress = new Tween(0, {
+		duration: 5000,
+		easing: cubicOut
+	});
 
 	onMount(() => {
 		visible = true;
-		function next() {
-			p += 0.1;
-			const remaining = 1 - p;
-			if (remaining > 0.15) setTimeout(next, 500 / remaining);
-		}
-		setTimeout(next, 250);
+		progress.target = 0.85;
 	});
 </script>
 
 {#if visible}
-	<div class="progress-container">
-		<div class="progress" style="width: {p * 100}%"></div>
-	</div>
-{/if}
-
-{#if p >= 0.4}
-	<div class="fade"></div>
+	<progress value={progress.current}></progress>
 {/if}
 
 <style>
-	.progress-container {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
+	progress {
+		border: none;
 		height: 4px;
-		z-index: 999;
-	}
-
-	.progress {
-		position: absolute;
-		left: 0;
-		top: 0;
-		height: 100%;
-		background-color: #5cb85c;
-		transition: width 0.4s;
-	}
-
-	.fade {
-		position: fixed;
+		display: block;
 		width: 100%;
-		height: 100%;
-		background-color: rgba(255, 255, 255, 0.3);
-		pointer-events: none;
-		z-index: 998;
-		animation: fade 0.4s;
-	}
-
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
+		background-color: #5cb85c;
+		position: absolute;
+		top: 0;
+		left: 0;
 	}
 </style>
